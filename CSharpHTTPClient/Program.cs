1197c7a1b55053940eff2ac288404da043337388
Elmer Thomas <elmer@thinkingserious.com>
2016-03-03 14:27:27 -0800

    First working prototype

---
+++ CSharpHTTPClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Dynamic;
+
+namespace CSharpHTTPClient
+{
+    public class Client : DynamicObject
+    {
+        public string url_path_name;
+        
+        public Client(string url_path_name = null)
+        {
+            if(url_path_name != null)
+            {
+                this.url_path_name = url_path_name;
+            }
+        }
+
+        // Magic method to handle special cases
+        public Client _(String magic)
+        {
+            this.url_path_name += magic + "/";
+            return new Client(this.url_path_name);
+        }
+
+        // Reflection
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            this.url_path_name += binder.Name + "/";
+            result = new Client(this.url_path_name);
+            return true;
+        }
+
+        // Catch final method call
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            if(binder.Name == "Get")
+            {
+                this.url_path_name += "GET";
+            }
+            result = new Client(this.url_path_name);
+            return true;
+        }
+    }
+}
---
+++ Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic client = new CSharpHTTPClient.Client();
+            dynamic temp = client.Foo;
+            dynamic ret = client.Foo.Bar.Will.Do._("magic").Get();
+            String string1 = ret.url_path_name;
+            Console.WriteLine("After first Get: " + string1);
+            dynamic ret2 = temp.Will.Do.Get();
+            String string2 = ret2.url_path_name;
+            Console.WriteLine("After second Get: " + string2);
+            Console.WriteLine("\n\nPress any key to continue.");
+            Console.ReadLine();
+        }
+    }
+}
