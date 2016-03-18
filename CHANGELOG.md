95e3fe00699eab86a21cfb05cf3214b7478705a1
Elmer Thomas <elmer@thinkingserious.com>
2016-03-17 22:13:07 -0700

    Version Bump 1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,6 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
-## [1.0.0] - 2016-XX-XX
+## [1.0.2] - 2016-03-17
 ### Added
 - We are live!
--- CSharpHTTPClient/Properties/AssemblyInfo.cs
+++ CSharpHTTPClient/Properties/AssemblyInfo.cs
@@ -1,17 +1,16 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SendGrid.CSharp.HTTP.Client")]
+[assembly: AssemblyDescription("A Simple Fluent REST API Client.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyCompany("SendGrid")]
+[assembly: AssemblyProduct("SendGrid.CSharp.HTTP.Client")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("SendGrid")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
