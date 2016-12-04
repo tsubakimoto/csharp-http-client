3310dc2aa90eec60bf6b31c84d20d333c4be7567
tsubakimoto <tkmbmbr@gmail.com>
2016-12-04 20:08:09 +0900

    convert CSharpHTTPClient to dotnet core

--- CSharpHTTPClient/Properties/AssemblyInfo.cs
+++ CSharpHTTPClient/Properties/AssemblyInfo.cs
@@ -1,35 +1,19 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SendGrid.CSharp.HTTP.Client")]
-[assembly: AssemblyDescription("A Simple Fluent REST API Client.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SendGrid")]
-[assembly: AssemblyProduct("SendGrid.CSharp.HTTP.Client")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("SendGrid")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpHTTPClient")]
+[assembly: AssemblyTrademark("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("26c4841f-ec62-4ec7-b16e-3a7386ea36dc")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: Guid("b6902050-2c2d-4b0b-83e5-1ba12148b313")]
